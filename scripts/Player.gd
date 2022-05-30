6d0d23020dedd4b89611edc74fd75653cba55bef
Tiago Treter <IceZombie13@users.noreply.github.com>
2022-05-30 15:52:10 -0300

    Atualizando arquivos do game.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,14 @@
 extends KinematicBody2D
 
+var speed = Vector2(0, 0)
+var gravity = Vector2(0, 200)
 
 func _ready():
 	pass
+	
+	
+func _physics_process(delta):
+	
+	speed = speed + gravity * delta
+	
+	move_and_slide(speed)

4deaeacbdd4d4c50392d14f39bd6ff181c109cd0
Tiago Treter <IceZombie13@users.noreply.github.com>
2022-05-30 16:04:44 -0300

    Update Player.gd
    O player agora pode se movimentar.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,14 +1,22 @@
 extends KinematicBody2D
 
-var speed = Vector2(0, 0)
-var gravity = Vector2(0, 200)
+var velocity = Vector2.ZERO
+var move_speed = 480
+var gravity = 1200
+var jump_force = -720
 
 func _ready():
 	pass
 	
 	
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
-	speed = speed + gravity * delta
+	if Input.is_action_pressed("move_jump"):
+		velocity.y = jump_force / 2
+		
+		
+	velocity.x = move_speed * move_direction
 	
-	move_and_slide(speed)
+	move_and_slide(velocity)

817e1c65be39773ec056139022511ff63568f2d4
Tiago Treter <IceZombie13@users.noreply.github.com>
2022-06-06 16:54:50 -0300

    Atualizando Game

---
+++ scenes/Tutorial.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _input(event):
+	if event.is_action_pressed("move_up") and len(get_overlapping_bodies()) > 0:
+		find_and_use_dialogues()
+		
+func find_and_use_dialogues():
+	var dialogue_player = get_node_or_null("Dialog")
+	
+	if dialogue_player:
+		dialogue_player.play()
